200818aeb34c711e5e44790510b4b06a1fe548c4
Adrian Zucco <adrigabzu@gmail.com>
2024-01-18 18:46:59 +0100

    Basic model using LightGBM

--- scripts/0_baseline_model.R
+++ scripts/0_baseline_model.R
@@ -1,7 +1,113 @@
+## Description: This script is used to create a baseline model for the health data.
+##
+## Author: Adrian G. Zucco and Tibor V. Varga
+## Date Created: 2024-01-18
+## Notes:
+## Inspiration from https://www.kaggle.com/code/athosdamiani/lightgbm-with-tidymodels
+
+## ------------------------------------
+
+
 library(skimr)
 library(tidyverse)
+library(tidymodels)
+library(rsample)
+library(lightgbm)
+library(parsnip)
+library(bonsai)
+library(tune)
+library(yardstick)
 
+# Seed for the baseline model
+seednr <- 02022024
+set.seed(seednr)
 
 # Load data
-data <- read_csv("../data/health_data.csv")
+rawdata <- read_csv("../data/health_data.csv")
 
+# Check data
+skim(rawdata)
+
+
+########### Pre-processing ################
+
+data <- rawdata %>%
+  # change format of day30 as factor
+  mutate(DAY30 = as.factor(DAY30))
+
+
+# Split data into train and test
+split_data <- initial_split(data, prop = 0.70)
+train <- training(split_data)
+test <- testing(split_data)
+
+
+# Data processing recipe
+modelrecipe <- recipe(DAY30 ~ ., data = train) %>%
+  # combine low frequency factor levels
+  recipes::step_other(all_nominal(), threshold = 0.01) %>%
+  # remove no variance predictors which provide no predictive information
+  recipes::step_nzv(all_nominal(), -all_outcomes()) %>%
+  prep()
+
+# Have a look
+skim(juice(prep(modelrecipe)))
+
+# Define model
+lightgbmmodel <- boost_tree(
+  trees = 1000,
+  learn_rate = tune(),
+  tree_depth = tune(),
+  min_n = tune(),
+  loss_reduction = tune(),
+  sample_size = 0.75,
+  mtry = 5
+) %>%
+  set_engine("lightgbm", objective = "binary", metric = "binary_logloss") %>%
+  set_mode("classification")
+
+
+# Fit model
+healthworkflow <- workflow() %>%
+  add_recipe(modelrecipe) %>%
+  add_model(lightgbmmodel)
+
+# Cross-validation
+resamples <- vfold_cv(train, v = 3)
+
+### Define hyperparameter optimization
+training_grid <- parameters(lightgbmmodel) %>%
+  finalize(train) %>%
+  grid_random(size = 20)
+head(training_grid)
+
+# grid search
+tune_grid <- healthworkflow %>%
+  tune_grid(
+    resamples = resamples,
+    grid = training_grid,
+    control = control_grid(verbose = FALSE),
+    metrics = metric_set(roc_auc, pr_auc, accuracy, brier_class),
+    control = control_grid(verbose = TRUE)
+  )
+
+# Show best models
+show_best(tune_grid, "pr_auc", n = 10)
+
+best_params <- select_best(tune_grid, "roc_auc")
+
+
+healthworkflow <- healthworkflow %>%
+  finalize_workflow(best_params)
+
+# Fit model on test set
+last_fit <- last_fit(healthworkflow,
+  split = split_data
+)
+
+test_preds <- collect_predictions(last_fit)
+test_preds
+
+# TODO Fix this
+multi_metric <- metric_set(roc_auc, pr_auc, brier_class)
+multi_metric(test_preds, truth = DAY30, estimate = .pred_1)
